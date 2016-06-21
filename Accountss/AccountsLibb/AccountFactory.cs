5ac7587b3144984601a68e445cecf252a5cd4bde
Mariana <Mariana@MARIANA>
2016-06-21 10:45:03 +0300

    lab 3.1

---
+++ Accountss/AccountsLibb/AccountFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountsLibb
+{
+    static class AccountFactory
+    {
+        static int runningID = 100;
+        public static Account CreateAccount(double initialBalance)
+        {
+            Account account = new Account(runningID, initialBalance);
+            account.Deposit(initialBalance);
+            runningID += 1;
+            if (account != null)
+            {
+                return account;
+            }
+            return null;
+            
+        }
+    }
+}
