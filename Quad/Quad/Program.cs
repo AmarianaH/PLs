f4f5d878a12705bffd35419959db750e9311f184
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-23 23:22:32 +0300

    2.4

--- Quad/Quad/Program.cs
+++ Quad/Quad/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Quad
 {
+    //You should read input from the command line arguments - from the 'args' argument you got in the Main method.
     class Program
     {
         static void Main(string[] args)
@@ -16,6 +17,9 @@ namespace Quad
             t3 = Console.ReadLine();
             // Console.WriteLine($" 1 {t1} , 2 {t2} , 3 {t3} ");
             double a, b, c;
+
+            //You can also use double.TryParse. It is the same. 'double' is an alias for Double. Each time you write 'double' it compiles down to System.Double.
+            //The same some int, uint, long, ulong, short, ushort, char, byte, ubyte, double, float, and Int32, Int64, Short, Char, Byte, Double, Float, etc...
             Double.TryParse(t1 ,out a);
             Double.TryParse(t2, out b);
             Double.TryParse(t3, out c);
