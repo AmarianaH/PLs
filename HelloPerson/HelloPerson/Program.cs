fd6e0cfd08a25ed53944aa0ffb9e0ab1af1c0acc
Moaid Hathot <moaid.hathot@outlook.com>
2016-07-23 23:10:20 +0300

    2.1

--- HelloPerson/HelloPerson/Program.cs
+++ HelloPerson/HelloPerson/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace HelloPerson
 {
+    //No input validation.
     class Program
     {
         static void Main(string[] args)
@@ -13,9 +14,13 @@ namespace HelloPerson
             string name ;
             Console.WriteLine("Type Your Name Please : ");
             name = Console.ReadLine();
-            Console.WriteLine($"Hello {name} , Welcome To My Project");
+
+            //C# 6 feature. Nice. (String interpolation)
+            Console.WriteLine($"Hello {name} , Welcome To My Project"); //ahleen
             Console.WriteLine("Please Enter Your Age: ");
             var temp = Console.ReadLine();
+
+            //according to the C# convention, variables should start with a small letter.
             var Age = int.Parse(temp);
             Console.WriteLine("Here's a space for each year of your life :) : ");
             for(int i=0; i < Age; i++){
